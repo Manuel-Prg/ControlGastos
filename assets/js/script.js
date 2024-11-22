07f2f35d9683a69e3c94d5305ab9aeb8f871e1a7
José Manuel Pérez Gutiérrez <ManuelPrz024@gmail.com>
2024-11-22 17:46:18 -0600

    se agregaron las funciones del desafio - inmersión dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,80 @@
+//DESAFIOS DE JAVASCRIPT:
+//Generar un mensaje de alerta cuando se registre un gasto mayor a 150$ dólares.
+//Agregar un nuevo campo donde se pueda colocar una descripción más detallada del gasto.
+//Agregar un botón que permita modificar los gastos registrados.
+
 let listaNombresDeGastos = [];
 let listasValoresGastos = [];
+let listasDescripcionGastos = [];
 
-//esta función se invoca al momento de hacer click en el boton
-function clickBoton(){
+function clickBoton() {
+  let nombreGasto = document.getElementById("nombreGasto").value;
+  let valorGasto = document.getElementById("valorGasto").value;
 
-    let nombreGasto = document.getElementById("nombreGasto").value;
-    let valorGasto = document.getElementById("valorGasto").value;
-    
-    listaNombresDeGastos.push(nombreGasto);
-    listasValoresGastos.push(valorGasto);
+  listaNombresDeGastos.push(nombreGasto);
+  listasValoresGastos.push(valorGasto);
 
-    actuiazarListaGastos();
+  actualizarListaGastos();
 }
 
-function actuiazarListaGastos(){
+function actualizarListaGastos() {
     const listaElementos = document.getElementById("listaDeGastos");
     const totalElemento = document.getElementById("totalGastos");
-
+  
     let htmlLista = '';
     let totalGastos = 0;
-
+  
     listaNombresDeGastos.forEach((gasto, index) => {
-        const valorGatos = Number(listasValoresGastos[index]);
-
-        htmlLista += `<li>${gasto.toFixed(2)}: US ${valorGatos}
-        <button  onclick="eliminarGasto(${index});">Eliminar Gasto</button>
-        </li>`
-
-        //calculamos el total de gastos
-        totalGastos += Number(valorGatos);
+      const valorGastos = Number(listasValoresGastos[index]);
+  
+      if (valorGastos <= 150) {
+        htmlLista += `<li>${gasto}: US ${valorGastos.toFixed(2)} - ${listasDescripcionGastos[index] || ''}
+          <button onclick="eliminarGasto(${index});">Eliminar</button>
+          <button onclick="modificarGasto(${index});">Modificar</button>
+          <button onclick="descripciónGasto(${index});">Descripción</button>
+        </li>`;
+  
+        totalGastos += valorGastos;
+      } 
+      else {
+        alert('No puedes gastar más de $150');
+      }
     });
-    
+
     listaElementos.innerHTML = htmlLista;
     totalElemento.innerHTML = totalGastos.toFixed(2);
-    limpiarFortmulario();
+    limpiarFormulario();
+  }
 
+  function modificarGasto(index) {
+    let nuevoNombre = prompt("Ingrese el nuevo nombre del gasto");
+    let nuevoValor = prompt("Ingrese el nuevo valor del gasto");
+  
+    listaNombresDeGastos[index] = nuevoNombre;
+    listasValoresGastos[index] = nuevoValor;
+  
+    actualizarListaGastos();
+  }
+
+  function descripciónGasto(index) {
+    let nuevaDescripcion = prompt("Ingrese la nueva descripción del gasto");
+  
+    if (nuevaDescripcion === " ") {
+        alert("por favor inntroduzca una descripción");
+        return;
+    }
+    listasDescripcionGastos[index] = nuevaDescripcion;
+  
+    actualizarListaGastos();
+  }
+
+function limpiarFormulario() {
+  document.getElementById("nombreGasto").value = '';
+  document.getElementById("valorGasto").value = '';
 }
 
-function limpiarFortmulario(){
-        document.getElementById("nombreGasto").value = '';
-        document.getElementById("valorGasto").value = '';
-}
-
-function eliminarGasto(index){
-    listaNombresDeGastos.splice(index,1);
-    listasValoresGastos.splice(index,1);
-    actuiazarListaGastos();
+function eliminarGasto(index) {
+  listaNombresDeGastos.splice(index, 1);
+  listasValoresGastos.splice(index, 1);
+  actualizarListaGastos();
 }
